b0521868ffca4c4315f322d7c05f7c95a5c0b207
linuxacademy-shiraz <shiraz@linuxacademy.com>
2017-03-28 14:29:47 -0500

    Add files via upload

---
+++ dynamodb-lab/dynamodb.py
@@ -0,0 +1,55 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+
+table = dynamodb.Table('Movies')
+
+with open("/home/linuxacademy/moviedata.json") as json_file:
+    movies = json.load(json_file, parse_float = decimal.Decimal)
+    for movie in movies:
+        year = int(movie['year'])
+        title = movie['title']
+        info = movie['info']
+
+        print("Adding movie:", year, title)
+
+        table.put_item(
+           Item={
+               'year': year,
+               'title': title,
+               'info': info,
+            }
+        )
