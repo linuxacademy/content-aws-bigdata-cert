407d91749bdf0c3c5b0ed0edde63f854d5f794c8
Fernando Medina Corey <fmcorey@gmail.com>
2018-10-04 12:37:31 -0700

    Clear up unused things, lint

--- course-lessons-project/project2-order-management-system/orders.py
+++ course-lessons-project/project2-order-management-system/orders.py
@@ -1,9 +1,7 @@
 import boto3
-import csv
 import uuid
 import random
 import time
-from collections import Counter
 
 from faker import Faker
 
@@ -41,6 +39,7 @@ while num_ids < 2000:
     order_ids.append(str(uuid.uuid4()))
     num_ids = num_ids + 1
 
+
 def create_order_data():
     manufacturer = random.choice(manufacturers)
     item = random.choice(items)
@@ -49,19 +48,23 @@ def create_order_data():
     order['item_id'] = str(uuid.uuid4())
     order['item_name'] = manufacturer[0] + ' ' + item[0]
     order['item_type'] = item[0]
-    order['item_sku'] = order['item_name'][0:4].upper().replace('-','')
+    order['item_sku'] = order['item_name'][0:4].upper().replace('-', '')
     order['color'] = fake.color_name()
     order['item_price'] = str(round(manufacturer[1] * item[1])+.99)
     order['item_tax'] = str(round((float(order['item_price']) * .03), 2))
-    order['item_total'] = str(float(order['item_price']) + float(order['item_tax']))
+    order['item_total'] = str(
+        float(order['item_price']) + float(order['item_tax'])
+    )
     order['item_manufacturer'] = manufacturer[0]
     order['customer_name'] = fake.name()
     order['customer_phone_number'] = fake.phone_number()
     return order
 
+
 def send_to_dynamodb(item):
     print('Sending item to DynamoDB: ' + str(item))
     table.put_item(Item=item)
+
 
 def write_orders():
     n = 0
@@ -70,29 +73,30 @@ def write_orders():
         n = n + 1
         # Create random order
         new_order = create_order_data()
-        # When true, used to occassionally throw 
+        # When true, used to occassionally throw
         # out things that aren't PenguinPolar
         condition1 = [
-            new_order['item_manufacturer'] != 'PenguinPolar', 
-            random.randint(1,101) > 70
+            new_order['item_manufacturer'] != 'PenguinPolar',
+            random.randint(1, 101) > 70
         ]
-        # When true, used to occassionally throw out 
+        # When true, used to occassionally throw out
         # things that aren't Sunglasses or Carabiners
         condition2 = [
-            new_order['item_type'] not in ['Sunglasses','Carabiner'],
-            random.randint(1,101) > 70
+            new_order['item_type'] not in ['Sunglasses', 'Carabiner'],
+            random.randint(1, 101) > 70
         ]
-        # When true, used to occassionally throw out 
+        # When true, used to occassionally throw out
         # Ice Pick and Crampons
         condition3 = [
-            new_order['item_type'] in ['Ice Pick','Crampon'],
-            random.randint(1,101) > 20
+            new_order['item_type'] in ['Ice Pick', 'Crampon'],
+            random.randint(1, 101) > 20
         ]
         if all(condition1) or all(condition2) or all(condition3):
             new_order = create_order_data()
             send_to_dynamodb(new_order)
         else:
             send_to_dynamodb(new_order)
-        
+
+
 if __name__ == '__main__':
     write_orders()
